fec5f2f4d2f7b5e110136e3ae2453c2683b61398
Raj <rajdristant007@gmail.com>
2023-06-07 16:07:32 +0530

    day 1 q1

---
+++ Day 1/Set Matrix Zeroes.cpp
@@ -0,0 +1,52 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+    void makeColumnZero(vector<vector<int>> &matrix, int y, int n)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            matrix[x][y] = 0;
+        }
+    }
+    void makeRowZero(vector<vector<int>> &matrix, int x, int m)
+    {
+        for (int y = 0; y < m; y++)
+        {
+            matrix[x][y] = 0;
+        }
+    }
+
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        queue<pair<int, int>> zeroPos;
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+        // Locating all the zeros and adding them to queue
+        for (int x = 0; x < n; x++)
+        {
+            for (int y = 0; y < m; y++)
+            {
+                if (matrix[x][y] == 0)
+                {
+                    zeroPos.push({x, y});
+                }
+            }
+        }
+
+        while (!zeroPos.empty())
+        {
+            int x = zeroPos.front().first;
+            int y = zeroPos.front().second;
+            zeroPos.pop();
+
+            makeColumnZero(matrix, y, n);
+            makeRowZero(matrix, x, m);
+        }
+    }
+};
