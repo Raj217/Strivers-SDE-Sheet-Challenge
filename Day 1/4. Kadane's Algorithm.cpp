377f2589939b335f1af64aaecc9c9dd8aa9ead11
Raj <rajdristant007@gmail.com>
2023-06-07 22:47:59 +0530

    day1 q4

---
+++ Day 1/4. Kadane's Algorithm.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // Implementing Kadane's Algorithm
+
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for (int num: nums) {
+            sum += num;
+
+            maxSum = max(maxSum, sum);
+            if (sum < 0) sum = 0; // Carying a negative sum will decrease the overall sum so of no use
+        }
+        return maxSum;
+    }
+};
