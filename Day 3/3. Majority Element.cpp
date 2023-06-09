573900c8bb1cba2bef46db2c8052dce8d82f807f
Raj <rajdristant007@gmail.com>
2023-06-09 21:53:50 +0530

    Day 3 q3

---
+++ Day 3/3. Majority Element.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int element = -1, count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (count == 0)
+                element = nums[i];
+
+            if (nums[i] == element)
+                count++;
+            else
+                count--;
+        }
+        return element;
+    }
+};
