1a7f7022b6a2b97db812307b20b4d554abb968c9
Raj <rajdristant007@gmail.com>
2023-06-09 21:37:21 +0530

    Day3 q2

---
+++ Day 3/2. Pow(x, n).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        long long p = abs(n);
+        double ans = 1;
+        while (p > 0)
+        {
+            if (p % 2 == 0)
+            {
+                x *= x;
+                p /= 2;
+            }
+            else
+            {
+                ans *= x;
+                p--;
+            }
+        }
+        if (n < 0)
+            return 1 / ans;
+        return ans;
+    }
+};
