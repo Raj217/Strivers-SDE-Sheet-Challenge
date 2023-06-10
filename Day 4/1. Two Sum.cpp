35c1d3a58a0e1f8fc742afaa9cd0ccb52f143022
Raj <rajdristant007@gmail.com>
2023-06-10 23:49:14 +0530

    Day 4 q1

---
+++ Day 4/1. Two Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> pos;
+
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            pos[nums[i]] = i;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (pos.find(target - nums[i]) != pos.end() && pos[target - nums[i]] != i)
+            {
+                return {i, pos[target - nums[i]]};
+            }
+        }
+        return {};
+    }
+};
