1c992cd5fa653ac529f6720724cfa9618f967e2a
Raj <rajdristant007@gmail.com>
2023-06-12 02:56:01 +0530

    Day 4 q4

---
+++ Day 4/4. Longest Subarray Zero Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LongestSubsetWithZeroSum(vector<int> arr)
+{
+
+    int maxLen = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int j = i;
+        int s = arr[j++];
+        if (s == 0)
+            maxLen = max(maxLen, 1);
+        while (j - i < maxLen)
+            s += arr[j++];
+        for (; j < arr.size(); j++)
+        {
+            s += arr[j];
+            if (s == 0)
+            {
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+}
