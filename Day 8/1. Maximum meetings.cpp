a0a9b16a3ca12c49cddbaf048d73fb79e37c1de3
Raj <rajdristant007@gmail.com>
2023-06-16 13:51:41 +0530

    Day 8 q1

---
+++ Day 8/1. Maximum meetings.cpp
@@ -0,0 +1,20 @@
+// https://www.codingninjas.com/codestudio/problems/1062658?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+vector<int> maximumMeetings(vector<int> &start, vector<int> &end) {
+    int n = start.size();
+    vector<pair<int, int>> timeStamps(n);
+    for (int i=0; i<n; i++) {
+        timeStamps[i] = {end[i], i+1};
+    }
+    sort(timeStamps.begin(), timeStamps.end());
+    int e=-1;
+    vector<int> meetings;
+    for (int i=0; i<n; i++) {
+        if (e < start[timeStamps[i].second-1]) {
+            e = timeStamps[i].first;
+            meetings.push_back(timeStamps[i].second);
+        }
+    }
+    return meetings;
+}
