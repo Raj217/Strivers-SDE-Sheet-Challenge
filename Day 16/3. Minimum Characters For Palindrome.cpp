b1a6cececf4886c9ec5a75c3e6501245fa9901fd
Raj <rajdristant007@gmail.com>
2023-06-26 01:09:41 +0530

    Day 16 q3

---
+++ Day 16/3. Minimum Characters For Palindrome.cpp
@@ -0,0 +1,24 @@
+// https://www.codingninjas.com/codestudio/problems/893000?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minCharsforPalindrome(string str) {
+	int n = str.size();
+    int low = 0;
+    int high = n - 1;
+    int e2 = high;
+    int cnt = 0;
+    while (low < high){
+        if (str[low] == str[high]){
+            low++;
+            high--;
+        }else{
+            cnt++;
+            low = 0;
+            e2--;
+            high = e2;
+        }
+    }
+    return cnt;
+}
