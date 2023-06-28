003403d4c22e8658b5fb4a767d551c0f88d435d4
Raj <rajdristant007@gmail.com>
2023-06-28 15:16:19 +0530

    Day 20 q4

---
+++ Day 20/4. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/description/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    TreeNode* build(vector<int> &preorder, vector<int> &inorder, int preStart, int preEnd, int inStart, int inEnd, unordered_map<int, int> &inorderPos) {
+        if (preStart > preEnd || inStart > inEnd) return NULL;
+
+        TreeNode *root = new TreeNode(preorder[preStart]);
+        
+        int inorderInd = inorderPos[preorder[preStart]];
+        int left = inorderInd - inStart;
+
+        root->left = build(preorder, inorder, preStart+1, preStart+left, inStart, inorderInd-1, inorderPos);
+        root->right = build(preorder, inorder, preStart+left+1, preEnd, inorderInd+1, preEnd, inorderPos);
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        vector<int> inorder = preorder;
+        unordered_map<int, int> inorderPos;
+        int n = inorder.size();
+
+        sort(inorder.begin(), inorder.end());
+        
+        for (int i=0; i<n; i++) {
+            inorderPos[inorder[i]] = i;
+        }
+        return build(preorder, inorder, 0, n-1, 0, n-1, inorderPos);
+    }
+};
